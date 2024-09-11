f28988953f4cc9491e340d10c0175c907084ff12
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-09-11 23:29:17 +0530

    code restructured in Main class

--- src/datastructures/linkedList/Main.java
+++ src/datastructures/linkedList/Main.java
@@ -1,45 +1,40 @@
 package datastructures.linkedList;
 
+
 public class Main {
     public static void main(String[] args) {
         LinkedList l1 = new LinkedList();
+        System.out.println("Initial Linked List");
         l1.append(10);
         l1.append(20);
         l1.append(30);
-        l1.prepend(5);
-        l1.prepend(0);
+        getInfo(l1);
+        System.out.println("Adding Elements using append() :");
+        l1.append(40);
+        getInfo(l1);
+        System.out.println("Adding Elements using prepend() :");
+        l1.prepend(50);
+        getInfo(l1);
+        System.out.println("Removing Last Element");
+        l1.removeLast();
+        getInfo(l1);
+        System.out.println("Removing First Element");
+        l1.removeFirst();
+        getInfo(l1);
+        System.out.println("Getting node through index");
+        System.out.println("Element at index 1: " + l1.get(1).value);
+        System.out.println("Inserting Node through index (1): ");
+        l1.insertAt(1, 25);
+        getInfo(l1);
+        System.out.println("Modifying value of index 1: ");
+        l1.set(1, 100);
+        getInfo(l1);
+    }
+
+    public static void getInfo(LinkedList l1) {
+        l1.printList();
         l1.getHead();
         l1.getTail();
         l1.getLength();
-        l1.printList();
-        System.out.println("Removing Last Element");
-        l1.removeLast();
-        l1.printList();
-        l1.getLength();
-        System.out.println("Removing First Element");
-        l1.removeFirst();
-        l1.printList();
-        l1.getLength();
-        l1.removeFirst();
-        l1.printList();
-        l1.removeFirst();
-        l1.printList();
-        l1.removeFirst();
-        l1.printList();
-        l1.removeFirst();
-        l1.printList();
-        l1.removeLast();
-        l1.printList();
-        l1.append(10);
-        l1.append(20);
-        l1.append(30);
-        System.out.println("Getting node through index");
-        //Node n = l1.get(1);
-        System.out.println(l1.get(1).value);
-        System.out.println("Inserting Node through index");
-        l1.insertAt(1, 25);
-        l1.printList();
-        l1.set(1,100);
-        l1.printList();
     }
 }
