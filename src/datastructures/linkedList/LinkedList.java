3d760e564b0644ad1c98293c5b19b526ee6fc68d
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-09-11 23:00:15 +0530

    code restructured

--- src/datastructures/linkedList/LinkedList.java
+++ src/datastructures/linkedList/LinkedList.java
@@ -4,64 +4,69 @@ public class LinkedList {
     private Node head;
     private Node tail;
     private int length;
+
     public void getHead() {
-        if (isEmpty()){
+        if (isEmpty()) {
             System.out.println("Empty Linked List");
             return;
         }
         System.out.println("Head: " + head.value);
     }
+
     public void getTail() {
-        if (isEmpty()){
+        if (isEmpty()) {
             System.out.println("Empty Linked List");
             return;
         }
         System.out.println("Tail: " + tail.value);
     }
+
     public void getLength() {
         System.out.println("Length: " + length);
     }
+
     public void printList() {
-        if (isEmpty()){
+        if (isEmpty()) {
             System.out.println("Empty Linked List");
             return;
         }
         Node temp = head;
-        while (temp!=null) {
+        while (temp != null) {
             System.out.print(temp.value + " => ");
-            temp=temp.nextNode;
+            temp = temp.nextNode;
         }
         System.out.println("null");
     }
+
     public void append(int value) {
         Node newNode = new Node(value);
-        if(isEmpty()) {
+        if (isEmpty()) {
             head = newNode;
-        }
-        else {
-            tail.nextNode=newNode;
+        } else {
+            tail.nextNode = newNode;
         }
         tail = newNode;
         length++;
     }
+
     public void prepend(int value) {
         Node newNode = new Node(value);
         if (isEmpty()) {
             head = newNode;
             tail = newNode;
-        }
-        else {
-            newNode.nextNode=head;
-            head=newNode;
+        } else {
+            newNode.nextNode = head;
+            head = newNode;
         }
         length++;
     }
-    public void  removeLast() {
-        if (isEmpty()){
+
+    public void removeLast() {
+        if (isEmpty()) {
             System.out.println("Empty Linked List");
             return;
         }
-        if (head==tail) {
+        if (head == tail) {
 
             head = null;
             tail = null;
@@ -70,36 +75,37 @@ public class LinkedList {
         }
         Node temp = head;
         Node pre = head;
-        while (temp.nextNode!=null){
-            pre=temp;
-            temp=temp.nextNode;
+        while (temp.nextNode != null) {
+            pre = temp;
+            temp = temp.nextNode;
         }
-        pre.nextNode=null;
-        tail=pre;
+        pre.nextNode = null;
+        tail = pre;
         length--;
 
     }
+
     public void removeFirst() {
-        if (isEmpty()){
+        if (isEmpty()) {
             System.out.println("Empty Linked List");
             return;
         }
-        if (head==tail) {
+        if (head == tail) {
             head = null;
             tail = null;
             length--;
             return;
         }
-        head=head.nextNode;
+        head = head.nextNode;
         length--;
 
     }
 
     public Node get(int index) {
-        if (index>length || index < 0) {
+        if (index > length || index < 0) {
             if (isEmpty()) {
                 System.out.println("Empty Linked List");
-            }else {
+            } else {
                 System.out.println("Invalid Index");
             }
             return null;
@@ -108,14 +114,14 @@ public class LinkedList {
         if (isEmpty()) {
             System.out.println("Empty Linked List");
             return null;
-        }else {
-            int tempIndex=0;
-            Node temp=head;
-            while (temp!=null){
-                if (tempIndex==index) {
+        } else {
+            int tempIndex = 0;
+            Node temp = head;
+            while (temp != null) {
+                if (tempIndex == index) {
                     return temp;
                 }
-                temp=temp.nextNode;
+                temp = temp.nextNode;
                 tempIndex++;
 
             }
@@ -124,43 +130,43 @@ public class LinkedList {
     }
 
     public void insertAt(int index, int value) {
-        if (index>length || index<0) {
+        if (index > length || index < 0) {
             if (isEmpty()) {
                 System.out.println("Empty Linked List");
-            }else {
+            } else {
                 System.out.println("Invalid Index");
             }
             return;
-        } else if (index==length-1){
+        } else if (index == length - 1) {
             append(value);
             length++;
             return;
-            }
-        else if (index==0) {
+        } else if (index == 0) {
             prepend(value);
             length++;
             return;
         }
         Node newNode = new Node(value);
-        Node temp = get(index-1);
-        newNode.nextNode=temp.nextNode;
-        temp.nextNode=newNode;
+        Node temp = get(index - 1);
+        newNode.nextNode = temp.nextNode;
+        temp.nextNode = newNode;
         length++;
     }
-public void set(int index, int value) {
-    if (index > length || index < 0) {
-        if (isEmpty()) {
-            System.out.println("Empty Linked List");
-        }else {
-            System.out.println("Invalid Index");
-        }
-        return;
-        }
-    Node temp = get(index);
-    temp.value = value;
-}
 
-public boolean isEmpty() {
-    return head == null;
-}
+    public void set(int index, int value) {
+        if (index > length || index < 0) {
+            if (isEmpty()) {
+                System.out.println("Empty Linked List");
+            } else {
+                System.out.println("Invalid Index");
+            }
+            return;
+        }
+        Node temp = get(index);
+        temp.value = value;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
 }
