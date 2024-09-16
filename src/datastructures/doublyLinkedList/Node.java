cf4ca8b4e413c0d96dafe32e402d364fc3acb9d1
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-09-16 23:13:24 +0530

    DLL private variables initialized

--- src/datastructures/doublyLinkedList/Node.java
+++ src/datastructures/doublyLinkedList/Node.java
@@ -1,10 +1,10 @@
 package datastructures.doublyLinkedList;
-
+//Node class
 public class Node {
     Node previousNode;
     int value;
     Node nextNode;
-
+// Constructor
     public Node(int value) {
         this.value = value;
         previousNode = null;
