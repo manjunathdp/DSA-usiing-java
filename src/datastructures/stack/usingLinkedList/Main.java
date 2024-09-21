8fe8bca69a6aee51a3e98d15fce7bd5c70b95a9b
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-09-22 01:33:18 +0530

    isEmpty(), getTop() and getLength() method added and tested in main method

---
+++ src/datastructures/stack/usingLinkedList/Main.java
@@ -0,0 +1,11 @@
+package datastructures.stack.usingLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        System.out.println(s.isEmpty());
+        s.getTop();
+        s.getLength();
+
+    }
+}
--- src/datastructures/stack/usingLinkedList/Stack.java
+++ src/datastructures/stack/usingLinkedList/Stack.java
@@ -4,4 +4,20 @@ public class Stack {
     private Node top;
     private int length;
 
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public void getTop() {
+        if (isEmpty()) {
+            System.out.println("Stack is Empty");
+            return;
+        }
+        System.out.println("Top: " + top.getValue());
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
 }
