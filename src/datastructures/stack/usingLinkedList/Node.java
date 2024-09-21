89046af507e11455750983d027fdce3da5b90bce
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-09-22 00:56:20 +0530

    Node created for stack data structure

---
+++ src/datastructures/stack/usingLinkedList/Node.java
@@ -0,0 +1,11 @@
+package datastructures.stack.usingLinkedList;
+
+public class Node {
+    private int value;
+    private Node nextNode;
+
+    public Node(int value) {
+        this.value = value;
+        nextNode = null;
+    }
+}
