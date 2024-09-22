1acfa0ec4c90ed3b0a8add8c27ffeb9559243d6f
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-09-23 00:19:45 +0530

    Queue using Linked List created and tested in Main class

--- src/datastructures/queue/usingLinkedList/Main.java
+++ src/datastructures/queue/usingLinkedList/Main.java
@@ -2,6 +2,18 @@ package datastructures.queue.usingLinkedList;
 
 public class Main {
     public static void main(String[] args) {
-
+        Queue q = new Queue();
+        q.enqueue(10);
+        q.printQueue();
+        q.enqueue(20);
+        q.printQueue();
+        q.peek();
+        q.enqueue(30);
+        q.printQueue();
+        System.out.println("Size: " + q.size());
+        q.dequeue();
+        q.printQueue();
+        q.peek();
+        System.out.println("Size: " + q.size());
     }
 }
--- src/datastructures/queue/usingLinkedList/Queue.java
+++ src/datastructures/queue/usingLinkedList/Queue.java
@@ -1,10 +1,67 @@
+/*
+enqueue() – Insertion of elements to the queue.
+dequeue() – Removal of elements from the queue.
+peek() or front()- Acquires the data element available at the front node of the queue without deleting it.
+rear() – This operation returns the element at the rear end without removing it.
+isFull() – Validates if the queue is full.
+isEmpty() – Checks if the queue is empty.
+size(): This operation returns the size of the queue i.e. the total number of elements it contains.
+ */
+
 package datastructures.queue.usingLinkedList;
 
 public class Queue {
-    private Node font;
+    private Node front;
     private Node rear;
     private int length;
 
+    public boolean isEmpty() {
+        return front == null;
+    }
+
+    public void peek() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return;
+        }
+        System.out.println("Queue Front Element: " + front.getValue());
+    }
+
+    public int size() {
+        return length;
+    }
+
+    public void enqueue(int value) {
+        Node newNode = new Node(value);
+        if (isEmpty()) {
+            front = newNode;
+        } else {
+            rear.setNextNode(newNode);
+        }
+        rear = newNode;
+        length++;
+    }
+
+    public void dequeue() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return;
+        }
+        front = front.getNextNode();
+        length--;
+        if (front == null) {
+            rear = null;
+        }
+    }
+
+    public void printQueue() {
+        Node temp = front;
+        while (temp != null) {
+            System.out.print(temp.getValue() + " ");
+            temp = temp.getNextNode();
+        }
+        System.out.println();
+    }
 
 
 }
