609490fe1078a8f15de44f6edd7ea3d10896cde1
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-09-22 23:18:37 +0530

    getter and setter initialize for node

--- src/datastructures/queue/usingLinkedList/Node.java
+++ src/datastructures/queue/usingLinkedList/Node.java
@@ -1,4 +1,22 @@
 package datastructures.queue.usingLinkedList;
 
 public class Node {
+    private int value;
+    private Node previousNode;
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    public void setPreviousNode(Node previousNode) {
+        this.previousNode = previousNode;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Node getPreviousNode() {
+        return previousNode;
+    }
 }
