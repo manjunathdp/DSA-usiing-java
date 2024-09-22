431b3c5adea464fa8e619c0e60d0f3e484c93407
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-09-22 23:44:00 +0530

    variables initialize for Node

--- src/datastructures/queue/usingLinkedList/Node.java
+++ src/datastructures/queue/usingLinkedList/Node.java
@@ -2,21 +2,21 @@ package datastructures.queue.usingLinkedList;
 
 public class Node {
     private int value;
-    private Node previousNode;
+    private Node nextNode;
 
     public Node(int value) {
         this.value = value;
     }
 
-    public void setPreviousNode(Node previousNode) {
-        this.previousNode = previousNode;
+    public void setNextNode(Node nextNode) {
+        this.nextNode = nextNode;
     }
 
     public int getValue() {
         return value;
     }
 
-    public Node getPreviousNode() {
-        return previousNode;
+    public Node getNextNode() {
+        return nextNode;
     }
 }
