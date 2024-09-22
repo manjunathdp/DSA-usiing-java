0bdd98a5774e2c1ef725ccfa60d97ddd69fe6bfa
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-09-22 22:05:00 +0530

    stack using Arrays completed

--- src/datastructures/stack/usingArray/Main.java
+++ src/datastructures/stack/usingArray/Main.java
@@ -3,7 +3,21 @@ package datastructures.stack.usingArray;
 public class Main {
     public static void main(String[] args) {
         Stack s = new Stack();
-        System.out.println(s);
-
+        s.printStack();
+        System.out.println();
+        s.pop();
+        System.out.println("Stack after Push:");
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        s.push(40);
+        s.push(50);
+        s.printStack();
+        System.out.println();
+        System.out.println("Popped Element: " + s.pop());
+        System.out.println("Stack: ");
+        s.printStack();
+        System.out.println("Top element:" + s.getTop());
+        System.out.println("Height of the stack: " + s.getHeight());
     }
 }
--- src/datastructures/stack/usingArray/Stack.java
+++ src/datastructures/stack/usingArray/Stack.java
@@ -1,4 +1,46 @@
 package datastructures.stack.usingArray;
 
 public class Stack {
+    private final int MAX = 100;
+    private int height;
+    private int top = -1;
+    int[] stack = new int[MAX];
+
+    public void push(int value) {
+        top++;
+        stack[top] = value;
+        height++;
+    }
+
+    public int pop() {
+        if (top == -1) {
+            System.out.println("Stack is Empty");
+            return -1;
+        } else {
+            int temp = stack[top];
+            top--;
+            height--;
+            return temp;
+        }
+    }
+
+    public void printStack() {
+        if (top==-1){
+            System.out.println("Stack is Empty");
+            return;
+        }
+        int temp = top;
+        while (temp!=-1) {
+            System.out.println(stack[temp]);
+            temp--;
+        }
+    }
+
+    public int getHeight(){
+        return height;
+    }
+
+    public int getTop() {
+        return stack[top];
+    }
 }
