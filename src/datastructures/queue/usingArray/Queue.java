c83f45401dae2df4e6beaf99902ff580a1516748
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-09-24 21:43:40 +0530

    Initial variables set for Queue using Array

---
+++ src/datastructures/queue/usingArray/Queue.java
@@ -0,0 +1,9 @@
+package datastructures.queue.usingArray;
+
+public class Queue {
+    final int MAX = 100;
+    int front = -1;
+    int rear = -1;
+    int length;
+    int[] queue = new int[MAX];
+}
