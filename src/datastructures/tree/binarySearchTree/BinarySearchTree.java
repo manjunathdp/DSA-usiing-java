3448302424d778a7afd94fbb1c8cdc6ac544f4f8
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-09-25 17:13:24 +0530

    insert Method created

--- src/datastructures/tree/binarySearchTree/BinarySearchTree.java
+++ src/datastructures/tree/binarySearchTree/BinarySearchTree.java
@@ -4,6 +4,36 @@ public class BinarySearchTree {
     private Node root;
 
     public Node getRoot() {
-       return root;
+        return root;
+    }
+
+    public void insert(int value) {
+        Node newNode = new Node(value);
+        if (root == null) {
+            root = newNode;
+            return;
+        }
+        Node temp = root;
+        while (true) {
+            if (temp.value == newNode.value) {
+                System.out.println("Duplicate Values can't be inserted into Binary Search Tree");
+                return;
+            }
+            if (newNode.value > temp.value) {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return;
+                } else {
+                    temp = temp.right;
+                }
+            } else {
+                if (temp.left==null) {
+                    temp.left=newNode;
+                    return;
+                } else {
+                    temp=temp.left;
+                }
+            }
+        }
     }
 }
